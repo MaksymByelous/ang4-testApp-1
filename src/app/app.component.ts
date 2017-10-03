8c1b3f1f29733f6b4a606c0501ee47dc4c376377
Maxym Belous <maxbelous@Kelsies-MacBook-Pro.local>
2017-10-03 09:40:11 +0300

    test commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,71 @@
 import { Component } from '@angular/core';
+import { TepmlateServiceService } from './tepmlate-service.service';
+import { trigger,state,style,transition,animate,keyframes } from '@angular/animations';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  animations: [
+    trigger('testAnimation', [
+      state('small', style({
+        transform: 'scale(1)'
+      })),
+      state('large', style({
+        transform: 'scale(1.5)'
+      })),
+      transition('small <=> large', animate('300ms ease-out', style({
+        transform: 'translateY(20px)'
+      })))
+    ]),
+    trigger('testAnimation_2', [
+      state('opac', style({
+        transform: 'scale(1)'
+      })),
+      state('opacityty', style({
+        transform: 'scale(1.5)'
+      })),
+      transition('small <=> large', animate('300ms ease-out', keyframes([
+        style({opacity: 0, transform: 'translateY: (-100%)', offset: 0}),
+        style({opacity: 1, transform: 'translateY: (50px)', offset: 0.5}),
+        style({opacity: 1, transform: 'translateY: (0)', offset: 1})
+      ])))
+    ])
+  ]
 })
 export class AppComponent {
+  constructor(private tepmlateServiceService:TepmlateServiceService) {
+
+  }
+  ngOnInit() {
+    console.log(this.tepmlateServiceService.myPlaces())
+  }
+  // state: string = 'small'
+  small = true
+  opac = true
+  get stateName() {
+    return this.small ? 'small' : 'large'
+  }
+  get stateName2() {
+    return this.opac ? 'opac' : 'opacityty'
+  }
+  toggleSmall() {
+    this.small = !this.small;
+  }
+  toggleOpac() {
+    this.opac = !this.opac;
+  }
+  myState = this.tepmlateServiceService.myState()
+  places = this.tepmlateServiceService.myPlaces()
+
   title = 'app';
+
+  // animateMe () {
+  //   this.state = (this.state === 'small' ? 'large' : 'small')
+  // }
+  // animateMeToo () {
+  //   this.state = (this.state === 'small' ? 'large' : 'small')
+  // }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,26 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { SecondTestComponentComponent } from './second-test-component/second-test-component.component';
+import { DataBindingComponent } from './data-binding/data-binding.component';
+import { EventHandelingComponent } from './event-handeling/event-handeling.component';
+import { CssBindingComponent } from './css-binding/css-binding.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { TepmlateServiceService } from './tepmlate-service.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SecondTestComponentComponent,
+    DataBindingComponent,
+    EventHandelingComponent,
+    CssBindingComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule
   ],
-  providers: [],
+  providers: [TepmlateServiceService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
